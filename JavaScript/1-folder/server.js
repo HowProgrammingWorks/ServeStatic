9a2594f97ae19480a0334a9f7c64a99488e486e8
tshemsedinov <timur.shemsedinov@gmail.com>
2019-07-04 01:46:37 +0300

    Add MIME types

--- JavaScript/1-folder/server.js
+++ JavaScript/1-folder/server.js
@@ -6,6 +6,15 @@ const path = require('path');
 
 const STATIC_PATH = path.join(process.cwd(), './static');
 
+const MIME_TYPES = {
+  html: 'text/html; charset=UTF-8',
+  js: 'application/javascript; charset=UTF-8',
+  css: 'text/css',
+  png: 'image/png',
+  ico: 'image/x-icon',
+  svg: 'image/svg+xml',
+};
+
 const serveFile = name => {
   const filePath = path.join(STATIC_PATH, name);
   if (!filePath.startsWith(STATIC_PATH)) {
@@ -20,6 +29,9 @@ const serveFile = name => {
 http.createServer((req, res) => {
   const { url } = req; //.substring(1)
   const name = url === '/' ? '/index.html' : url;
+  const fileExt = path.extname(name).substring(1);
+  const mimeType = MIME_TYPES[fileExt] || MIME_TYPES.html;
+  res.writeHead(200, { 'Content-Type': mimeType });
   const stream = serveFile(name);
   if (stream) stream.pipe(res);
 }).listen(8000);
