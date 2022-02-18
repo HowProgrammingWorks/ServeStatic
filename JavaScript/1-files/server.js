c19d5db2860dd4cf122cb1cc61b49b5e87866cfb
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-02-18 15:44:54 +0200

    Improve example

--- JavaScript/1-files/server.js
+++ JavaScript/1-files/server.js
@@ -4,28 +4,33 @@ const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
-const STATIC_PATH = path.join(process.cwd(), './static');
+const PORT = 8000;
 
 const MIME_TYPES = {
+  default: 'application/octet-stream',
   html: 'text/html; charset=UTF-8',
   js: 'application/javascript; charset=UTF-8',
   css: 'text/css',
   png: 'image/png',
+  jpg: 'image/jpg',
+  gif: 'image/gif',
   ico: 'image/x-icon',
   svg: 'image/svg+xml',
 };
 
-const access = (file) => fs.promises.access(file).then(() => true, () => false);
+const STATIC_PATH = path.join(process.cwd(), './static');
+
+const toBool = [() => true, () => false];
 
 const prepareFile = async (url) => {
   const paths = [STATIC_PATH, url];
   if (url.endsWith('/')) paths.push('index.html');
   const filePath = path.join(...paths);
   const pathTraversal = !filePath.startsWith(STATIC_PATH);
-  const exists = await access(filePath);
+  const exists = await fs.promises.access(filePath).then(...toBool);
   const found = !pathTraversal && exists;
   const streamPath = found ? filePath : STATIC_PATH + '/404.html';
-  const ext = path.extname(streamPath).substring(1);
+  const ext = path.extname(streamPath).substring(1).toLowerCase();
   const stream = fs.createReadStream(streamPath);
   return { found, ext, stream };
 };
@@ -33,8 +38,10 @@ const prepareFile = async (url) => {
 http.createServer(async (req, res) => {
   const file = await prepareFile(req.url);
   const statusCode = file.found ? 200 : 404;
-  const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.html;
+  const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
   res.writeHead(statusCode, { 'Content-Type': mimeType });
   file.stream.pipe(res);
   console.log(`${req.method} ${req.url} ${statusCode}`);
-}).listen(8000);
+}).listen(PORT);
+
+console.log(`Server running at http://127.0.0.1:${PORT}/`);
